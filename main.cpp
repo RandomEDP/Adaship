f237b798b248d985f2b38b0020b15344b7a0444e
Ethan D-P <ethan.davison-pearce@ada.ac.uk>
2021-03-02 14:37:40 +0000

    Config Reading

--- main.cpp
+++ main.cpp
@@ -1,5 +1,66 @@
 #include <iostream>
+#define MINI_CASE_SENSITIVE
+#include "src/mini/ini.h"
+using namespace std;
+
+void computerGame(){
+  // first, create a file instance
+  mINI::INIFile file("adaship_config.ini");
+
+  // next, create a structure that will hold data
+  mINI::INIStructure ini;
+
+  // now we can read the file
+  file.read(ini);
+
+  int x = stoi(ini["Board"]["x"]);
+  int y = stoi(ini["Board"]["y"]);
+
+  class Ship{
+	  //Ship name
+    public:
+	  string name;
+	  //Total points on the grid
+  	int length;
+	  //Coordinates of those points
+    int x;
+    int y;
+    string direction;
+  };
+  cout << "You have choosen a computer game\n";
+  // int board[stoi(x)][stoi(y)]; // Two-dimensional array for gameboard.
+  int counter = 0;
+  for (auto const& it : ini){
+	  auto const& collection = it.second;
+	  for (auto const& it2 : collection){
+		  auto const& key = it2.first;
+      Ship ship[counter];
+      ship[counter].name = key;
+      int length = stoi(ini["Boats"][key]);
+      ship[counter].length = length;
+      cout << "Where would you like to place your " << key;
+	  }
+  }
+}
+
+void menu(){
+  int value;
+  cout << "Welcome to Adaship!\n";
+  cout << "1. One player v computer game\n";
+  cout << "2. Quit\n";
+  cin >> value;
+  if(value==1){
+    computerGame();
+  }
+  else if(value==2){
+    quick_exit(1);
+  }
+  else{
+    cout << "Please type 1 or 2";
+    menu();
+  }
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+  menu();
 }
